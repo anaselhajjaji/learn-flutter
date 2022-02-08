5c2bf5d8b3fa0be1d9eed82c9f7f8898df5da4a7
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-08 03:51:36 +0000

    use list view

--- personal_expenses_app/lib/widgets/transaction_list.dart
+++ personal_expenses_app/lib/widgets/transaction_list.dart
@@ -10,40 +10,44 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transactions.map((tx) {
-        //TODO: to build multiple widgets per transaction list we use map().
-        return Card(
-          child: Row(children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.purple, width: 2)),
-              padding: EdgeInsets.all(10),
-              child: Text('\$${tx.amount}', //TODO: called string interpolation
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple)),
-            ),
-            Column(
-              //TODO: For a column crossAxisAlignment is from left to right and for a row it's from top to bottom
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  tx.title,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  //TODO: I used intl package that supports a lot of date formatting patterns
-                  DateFormat.yMMMd().format(tx.date),
-                  style: TextStyle(color: Colors.grey),
-                )
-              ],
-            )
-          ]),
-        );
-      }).toList(),
+    return Container(
+      height: 300,
+      child: ListView(
+        children: transactions.map((tx) {
+          //TODO: to build multiple widgets per transaction list we use map().
+          return Card(
+            child: Row(children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2)),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                    '\$${tx.amount}', //TODO: called string interpolation
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple)),
+              ),
+              Column(
+                //TODO: For a column crossAxisAlignment is from left to right and for a row it's from top to bottom
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    //TODO: I used intl package that supports a lot of date formatting patterns
+                    DateFormat.yMMMd().format(tx.date),
+                    style: TextStyle(color: Colors.grey),
+                  )
+                ],
+              )
+            ]),
+          );
+        }).toList(),
+      ),
     );
   }
 }
