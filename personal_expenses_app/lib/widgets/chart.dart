484ad77f7d018c72c17726ca1647093f6f5dc0d5
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-08 18:05:33 +0000

    adds chart bar

--- personal_expenses_app/lib/widgets/chart.dart
+++ personal_expenses_app/lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import '../widgets/chart_bar.dart';
 import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -22,7 +23,17 @@ class Chart extends StatelessWidget {
         }
       }
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': totalSum
+      };
+    });
+  }
+
+  double get totalSpending {
+    //TODO: fold() will change a list to another type
+    return groupedTransactionsValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
     });
   }
 
@@ -33,7 +44,22 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(children: []),
+      child: Row(
+          //TODO: in a row the main axis alignement is from left to right
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionsValues.map((data) {
+            //TODO: to force a bar to use the available, Flexible can help to distribute the size in a row or column
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: data['day'] as String,
+                spendingAmount: data['amount'] as double,
+                spendingPercentageOfTotal: totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending,
+              ),
+            );
+          }).toList()),
     );
   }
 }
---
+++ personal_expenses_app/lib/widgets/chart_bar.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageOfTotal;
+
+  const ChartBar(
+      {Key? key,
+      required this.label,
+      required this.spendingAmount,
+      required this.spendingPercentageOfTotal})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('\$${spendingAmount.toStringAsFixed(2)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          //TODO: Stack Widget overlaps widgets
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+              //TODO: FractionallySizedBox a box with sized percentage
+              FractionallySizedBox(
+                heightFactor: spendingPercentageOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label)
+      ],
+    );
+  }
+}
