145f3ea56e37cbd1b82c2a7add50f5c83ac31668
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-08 21:38:17 +0000

    small fix

--- personal_expenses_app/lib/widgets/transaction_list.dart
+++ personal_expenses_app/lib/widgets/transaction_list.dart
@@ -13,7 +13,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 400,
       child: transactions.isEmpty
           //TODO: draw a waiting image when there is no transaction
           ? Column(
