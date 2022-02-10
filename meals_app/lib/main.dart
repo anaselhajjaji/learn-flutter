061881eb77f9ee43dd41837d495e51ef81ec3350
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-10 02:47:04 +0000

    adds some theming

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -3,18 +3,42 @@ import 'package:flutter/material.dart';
 import './categories_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final ThemeData theme =
+      /*TODO: we need to use primarySwatch instead of primaryColor 
+          because primary swatch will generate the other colors coming from primary color */
+      ThemeData(
+    primarySwatch: Colors.red,
+    canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+    fontFamily: 'Rowdies',
+  );
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Best Meals App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: theme.copyWith(
+        /*TODO: this is how we set accent color, the property is deprecated in ThemeData */
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: Colors.orange,
+        ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              /*bodyText1: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),*/
+              titleMedium: const TextStyle(
+                fontFamily: 'SupermercadoOne',
+                color: Colors.black,
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
