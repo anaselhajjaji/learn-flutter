4b79b1d95cf3aeac815d64306fcdf713dacda04a
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-10 21:29:13 +0000

    adds meals item

--- meals_app/lib/screens/category_meals_screen.dart
+++ meals_app/lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/meal_item.dart';
 import '../models/meal.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
@@ -34,7 +35,12 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
         },
         itemCount: categoryMeals.length,
       ),
---
+++ meals_app/lib/widgets/meal_item.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  void selectMeal() {}
+
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  const MealItem(
+      {Key? key,
+      required this.title,
+      required this.imageUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
+      imageurl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(children: [
+          Stack(
+            children: [
+              //TODO: will be used to clip the image
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(
+                  imageUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
