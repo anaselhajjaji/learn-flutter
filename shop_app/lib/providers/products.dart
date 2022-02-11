be4ffc8086e2def1661e2d836cd2bde0d00b9f35
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-11 21:27:55 +0000

    additional stuff to the provider

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 import '../models/product.dart';
 
-// TODO provider that will be used to manage the state in the application
+//TODO provider that will be used to manage the state in the application
+// which is called a mixin and it is just sharing methods and properties between classes
+// without strong relation between them (inheritence for example)
 class Products with ChangeNotifier {
   List<Product> _items = Product.getDummyProducts();
 
   List<Product> get items {
     // TODO return the copy of the items
     return [..._items];
+  }
+
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
   }
 }
--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -8,10 +11,12 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
-
+    //TODO listen, default is true. When we set to false, if Products provider changes, the widget will ne be rebuilt
+    final product =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(product.title),
       ),
     );
   }
