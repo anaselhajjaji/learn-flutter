7a902d32525b2dc795eca67a89e7eb259a6b8eaf
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-02-28 21:17:25 +0000

    adds form submission

--- shop_app/lib/screens/edit_product_screen.dart
+++ shop_app/lib/screens/edit_product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -12,6 +13,11 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _imageUrlController = TextEditingController();
   final _imageUrlFocusNode = FocusNode();
+  var _editedProduct =
+      Product(id: '', title: '', description: '', price: 0, imageUrl: '');
+
+  //TODO the globalkey will help us to get access to the form
+  final _form = GlobalKey<FormState>();
 
   //TODO used to manage focus when next is pushed on soft keyboard
   // to be confirmed but maybe not necessary in newer versions of flutter
@@ -41,16 +47,30 @@ class _EditProductScreenState extends State<EditProductScreen> {
     }
   }
 
+  void _saveForm() {
+    //TODO Save will save the form, so it will trigger a onSave method on each textformfield
+    _form.currentState?.save();
+
+    //TODO Here we have a complete product object
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Edit Product'),
+        actions: [
+          IconButton(
+            onPressed: _saveForm,
+            icon: const Icon(Icons.save),
+          ),
+        ],
       ),
       // TODO to handle all the input features, it's recommended to use Form widget
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          key: _form,
           //TODO Using ListView, user input might get lost if an input fields scrolls out of view
           // because the ListView widget dynamically removes and re-adds widgets as they scroll out of and back into view.
           // In that case Column with SingleChildScrollView should be used.
@@ -65,6 +85,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: value as String,
+                    description: _editedProduct.description,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(
@@ -73,6 +102,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: _editedProduct.title,
+                    description: _editedProduct.description,
+                    price: double.parse(value as String),
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               TextFormField(
                 decoration: const InputDecoration(
@@ -80,6 +118,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 ),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
+                onSaved: (value) {
+                  _editedProduct = Product(
+                    id: '',
+                    title: _editedProduct.title,
+                    description: value as String,
+                    price: _editedProduct.price,
+                    imageUrl: _editedProduct.imageUrl,
+                  );
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -113,9 +160,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       keyboardType: TextInputType.url,
                       controller: _imageUrlController,
                       focusNode: _imageUrlFocusNode,
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
                       onEditingComplete: () {
                         //TODO to trigger the UI update in order to render the image preview
                         setState(() {});
+                      },
+                      onSaved: (value) {
+                        _editedProduct = Product(
+                          id: '',
+                          title: _editedProduct.title,
+                          description: _editedProduct.description,
+                          price: _editedProduct.price,
+                          imageUrl: value as String,
+                        );
                       },
                     ),
                   ),
