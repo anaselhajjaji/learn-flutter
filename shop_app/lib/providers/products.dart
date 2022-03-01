567882f7c1b53d909c27effc40990ce225929e32
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-03-01 20:07:43 +0000

    adds delete product

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -41,4 +41,9 @@ class Products with ChangeNotifier {
       notifyListeners();
     }
   }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
+    notifyListeners();
+  }
 }
