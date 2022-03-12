a23f1b28a4932d72f8b7e436141dcc1e30c58440
Anas EL HAJJAJI <elhajjaji.anas@gmail.com>
2022-03-12 13:33:47 +0000

    save favorites

--- online_shop_app/lib/providers/product.dart
+++ online_shop_app/lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,9 +19,32 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = Uri.parse(
+        'https://flutter-shop-app-1e327-default-rtdb.firebaseio.com/products/$id.json');
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+
+      if (response.statusCode >= 400) {
+        // Rollback: because http patch returning 400 does not throw an exception
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      // Rollback
+      _setFavValue(oldStatus);
+    }
   }
 
   static List<Product> getDummyProducts() {
